21f49ae688863ec82b893e20cc6a95dae557b7b4
Nyeem Davis <89734265+NyeemDavis@users.noreply.github.com>
2023-01-21 07:11:00 -0500

    Update addStats.js

--- public/js/addStats.js
+++ public/js/addStats.js
@@ -1,1 +1,11 @@
 
+module.exports = {
+  applyStats = function checkClassAndMatch(charClass, obj) {
+    for(key in obj) {
+        if(charClass == key) {
+            console.log(`matches with ${key}`)
+            return key
+        }
+    }
+}
+}
