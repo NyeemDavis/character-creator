e617f8e9e488780e010f722eaf1bc2a46cd479f7
Nyeem Davis <davisnyeem@gmail.com>
2023-01-21 06:05:46 -0500

    changed stats from an array of objects to just an with different keys with the stats assigned to those keys

--- server.js
+++ server.js
@@ -3,19 +3,16 @@ const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('mongodb');
 const app = express();
 const PORT = 8000;
-const stats = require('./public/js/stats')
 require('dotenv').config()
 
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'characters'
 
-let statsdbName = 'stats'
 MongoClient.connect(dbConnectionString, { useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to databse')
         db = client.db(dbName)
-        statsdbName = client.db(statsdbName)
     })
 
 app.set('view engine', 'ejs');
@@ -52,14 +49,6 @@ app.post('/addCharacter', (req, res) => {
             res.redirect('/') // Redirect user to main page
         })
         .catch(err => console.error(err))
-}, () => {
-    app.put('/updateWithStats', (req, res) => {
-        console.log(req)
-        .then(result => {
-            console.log('Success')
-        })
-        .catch(err => console.error(err))
-    })
 })
 
 app.delete('/deleteCharacater', (req, res) => {
@@ -78,13 +67,32 @@ app.listen(process.env.PORT || PORT, () => {
 
 
 
-const stats = 
-   [{Warrior: {health: 200, defense: 175, damage: 150, magic: 20}},
-    {Archer : {health: 180,'defense': 120,'damage': 125,'magic': 30}},
-   {Support: {'health': 200,'defense': 175,'damage': 150,'magic': 60}},
-   {Mage: {'health': 200,'defense': 140,'damage': 75,'magic': 150}},
-   {Assassin: {'health': 200,'defense': 135,'damage': 165,'magic': 55 }},
-   {Healer: {'health': 200,'defense': 100,'damage': 60,'magic': 100}}]
+// const stats = 
+//    [{Warrior: {health: 200, defense: 175, damage: 150, magic: 20}},
+//     {Archer : {health: 180,'defense': 120,'damage': 125,'magic': 30}},
+//    {Support: {'health': 200,'defense': 175,'damage': 150,'magic': 60}},
+//    {Mage: {'health': 200,'defense': 140,'damage': 75,'magic': 150}},
+//    {Assassin: {'health': 200,'defense': 135,'damage': 165,'magic': 55 }},
+//    {Healer: {'health': 200,'defense': 100,'damage': 60,'magic': 100}}]
 
-
+const stats = {
+    Warrior: {
+        health: 200
+    },
+    Archer: {
+        health: 180
+    },
+    Support: {
+        health: 170
+    },
+    Mage: {
+        health: 150
+    },
+    Assassin: {
+        health: 195
+    },
+    Healer: {
+        health: 140
+    }
+}
 
