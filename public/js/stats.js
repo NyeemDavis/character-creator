a91d63add4ad3a99d6f73d5d74bf43f4df68bdbd
Nyeem Davis <davisnyeem@gmail.com>
2023-01-21 06:53:43 -0500

    blah blah blah

--- public/js/stats.js
+++ public/js/stats.js
@@ -9,13 +9,5 @@ async function applyStats() {
           }
      }
 }
-const stats = 
-   [{Warrior: {health: 200, defense: 175, damage: 150, magic: 20}},
-    {Archer : {health: 180,'defense': 120,'damage': 125,'magic': 30}},
-   {Support: {'health': 200,'defense': 175,'damage': 150,'magic': 60}},
-   {Mage: {'health': 200,'defense': 140,'damage': 75,'magic': 150}},
-   {Assassin: {'health': 200,'defense': 135,'damage': 165,'magic': 55 }},
-   {Healer: {'health': 200,'defense': 100,'damage': 60,'magic': 100}}]
-
 
 
