a1b4837e8a67517ed2b8976ddca2e1630eef32bf
Nyeem Davis <nyeemdavis4@gmail.com>
2023-01-30 21:58:31 -0500

    cleaned up code

--- public/js/weapons.js
+++ public/js/weapons.js
@@ -1,31 +1,33 @@
-module.exports = {weapons: Weapons = {
-    Swords: {
-     long_sword: {
-        damage: 100
-     },
-     short_sword: {
-        damage: 100
-     }   
-    },
-    Daggers: {
-        short_dagger: {
-            damage: 100
-        }
-    },
-    Bows: {
-        long_bow: {
+module.exports =  
+    Weapons = {
+        Swords: {
+        long_sword: {
             damage: 100
         },
-        compact_bow: {
-            damge: 100
-        }
-    },
-    Wands: {
-        crystal_wand: {
+        short_sword: {
             damage: 100
+        }   
         },
-        wooden_wand: {
-            damage: 100
+        Daggers: {
+            short_dagger: {
+                damage: 100
+            }
+        },
+        Bows: {
+            long_bow: {
+                damage: 100
+            },
+            compact_bow: {
+                damge: 100
+            }
+        },
+        Wands: {
+            crystal_wand: {
+                damage: 100
+            },
+            wooden_staff: {
+                damage: 100
+            }
         }
-    }
-}}
+}
+
