a84942c0b2755cd38fcb7c3a93d26614c8f4913b
Nyeem Davis <davisnyeem@gmail.com>
2023-02-05 00:24:06 -0500

    changed getStats to a function inside variable, changed from try catch then then catch

--- public/js/main.js
+++ public/js/main.js
@@ -22,43 +22,40 @@ Array.from(deleteBtn).forEach((el)=>{
   el.addEventListener('click', deleteCharacater)
 })
 
-Array.from(configBtn).forEach((el) => {
-  el.addEventListener('click', getStats)})
-
-
-async function getStats () {
+async function deleteCharacater(){
   const characterId = this.parentNode.dataset.id
-  try {
-    const response = await fetch('characters/config', {
-      method: 'put',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        characterIdFromJs: characterId
-      })
-    })
-    const data = await response.json()
-    console.log(data)
-    location.reload() 
-  } catch (err) {
-    console.log(err)
+  try{
+      const response = await fetch('characters/deleteCharacter', {
+          method: 'delete',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'characterFromJSFile': characterId
+          })
+        })
+      const data = await response.json()
+      console.log(data)
+      location.reload()
+  }catch(err){
+      console.log(err)
   }
 }
 
-async function deleteCharacater(){
-    const characterId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('characters/deleteCharacter', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'characterFromJSFile': characterId
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
+
+const getStats = async function () {
+  const characterId = this.parentNode.dataset.id
+  const response = 
+  await fetch(`/characters/config?${new URLSearchParams(characterId).toString()}`)
+    .then(res => res.json())
+    .then(configHTML => {
+      
+      location.reload()
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
+
+Array.from(configBtn).forEach((el) => {
+  el.addEventListener('click', getStats)})
+
 
